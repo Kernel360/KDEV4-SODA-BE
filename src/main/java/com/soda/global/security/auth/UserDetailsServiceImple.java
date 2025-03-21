becf5462e12c6787d71701a2f63a516a9896993e
yoodab <yoondab2166@gmail.com>
2025-03-21 12:47:59 +0900

    feat: Security 설정 및 JWT 인증 필터 추가(#3)

---
+++ src/main/java/com/soda/global/security/auth/UserDetailsServiceImple.java
@@ -0,0 +1,26 @@
+package com.soda.global.security.auth;
+
+import com.soda.global.response.ErrorCode;
+import com.soda.global.response.GeneralException;
+import com.soda.member.entity.Member;
+import com.soda.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImple implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String authId) throws UsernameNotFoundException {
+        Member member = memberRepository.findByAuthId(authId)
+                .orElseThrow(() -> new GeneralException(ErrorCode.NOT_FOUND));
+        return new UserDetailsImpl(member);
+    }
+}

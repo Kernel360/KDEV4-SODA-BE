3ed7b46af17202c9fd789510f1a0010999843a4a
yoodab <yoondab2166@gmail.com>
2025-03-21 13:54:15 +0900

    feat: 생성일자, 수정일자 자동 등록 수정(#3)

--- src/main/java/com/soda/SodaApplication.java
+++ src/main/java/com/soda/SodaApplication.java
@@ -2,8 +2,10 @@ package com.soda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SodaApplication {
 
 	public static void main(String[] args) {

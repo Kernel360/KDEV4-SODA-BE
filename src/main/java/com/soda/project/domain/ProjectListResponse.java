6d9e63a0050ba912502ae2bebd0eefec0938ee67
seoyeon-jung <jungluchia1204@gmail.com>
2025-03-21 12:47:29 +0900

    feat: 전체 프로젝트 조회 시 response 추가 (#1)

---
+++ src/main/java/com/soda/project/domain/ProjectListResponse.java
@@ -0,0 +1,22 @@
+package com.soda.project.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProjectListResponse {
+
+    private String title;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String devCompanyName;
+    private String clientCompanyName;
+
+}

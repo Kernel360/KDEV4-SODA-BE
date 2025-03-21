3a1780b136d3a471c4669b459853835d8cac6c2d
yoodab <yoondab2166@gmail.com>
2025-03-21 13:55:16 +0900

    feat: member 관련 ErrorCode 추가 (#3)

--- src/main/java/com/soda/global/response/ErrorCode.java
+++ src/main/java/com/soda/global/response/ErrorCode.java
@@ -16,7 +16,21 @@ public enum ErrorCode {
     SERVICE_UNAVAILABLE("503", "Service Unavailable: The server is temporarily unable to handle the request.", HttpStatus.SERVICE_UNAVAILABLE),
 
     // 공통 오류 메시지
-    UNEXPECTED_ERROR("1000", "Unexpected Error: An unexpected error occurred.", HttpStatus.INTERNAL_SERVER_ERROR);
+    UNEXPECTED_ERROR("1000", "Unexpected Error: An unexpected error occurred.", HttpStatus.INTERNAL_SERVER_ERROR), // 요청 관련 오류
+
+    // 인증 관련 추가 오류
+    DUPLICATE_AUTH_ID("2005", "이미 사용 중인 아이디입니다.", HttpStatus.BAD_REQUEST),
+
+    NOT_FOUND_REFRESH_TOKEN("2006", "Refresh token을 찾을 수 없습니다.", HttpStatus.UNAUTHORIZED),
+    INVALID_REFRESH_TOKEN("2007", "유효하지 않은 Refresh token입니다.", HttpStatus.UNAUTHORIZED),
+    NOT_FOUND_AUTH_ID("2005", "멤버를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
+
+
+    // 인증 관련 오류
+    INVALID_CREDENTIALS("2001", "아이디 또는 비밀번호가 올바르지 않습니다.", HttpStatus.UNAUTHORIZED),
+    TOKEN_EXPIRED("2002", "토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED),
+    INVALID_TOKEN("2003", "잘못된 토큰입니다.", HttpStatus.UNAUTHORIZED),
+    ACCESS_DENIED("2004", "접근 권한이 없습니다.", HttpStatus.FORBIDDEN);
 
     private final String code;
     private final String message;

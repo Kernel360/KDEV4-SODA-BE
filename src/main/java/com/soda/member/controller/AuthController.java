7bb8173b533a81b518e09ab610b87855e5de1acd
yoodab <yoondab2166@gmail.com>
2025-03-24 12:37:55 +0900

    feat: 이메일인증, 비밀번호 변경 컨트롤러 생성(#7)

--- src/main/java/com/soda/member/controller/AuthController.java
+++ src/main/java/com/soda/member/controller/AuthController.java
@@ -1,14 +1,15 @@
 package com.soda.member.controller;
 
+import com.soda.common.mail.dto.EmailRequest;
 import com.soda.global.response.ApiResponseForm;
-import com.soda.member.dto.LoginRequestDto;
-import com.soda.member.dto.LoginResponseDto;
-import com.soda.member.dto.SignupRequestDto;
+import com.soda.member.dto.*;
 import com.soda.member.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,20 +18,39 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<ApiResponseForm<Void>> signup(@RequestBody SignupRequestDto requestDto) {
+    public ResponseEntity<ApiResponseForm<Void>> signup(@RequestBody SignupRequest requestDto) {
         authService.signup(requestDto);
         return ResponseEntity.ok(ApiResponseForm.success(null, "회원가입 성공"));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ApiResponseForm<LoginResponseDto>> login(@RequestBody LoginRequestDto requestDto, HttpServletRequest request) {
-        LoginResponseDto token = authService.login(requestDto,request);
+    public ResponseEntity<ApiResponseForm<LoginResponse>> login(@RequestBody LoginRequest requestDto, HttpServletRequest request) {
+        LoginResponse token = authService.login(requestDto, request);
         return ResponseEntity.ok(ApiResponseForm.success(token, "로그인 성공"));
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<ApiResponseForm<LoginResponseDto>> refresh(HttpServletRequest request) {
-        LoginResponseDto responseDto = authService.refreshAccessToken(request);
+    public ResponseEntity<ApiResponseForm<LoginResponse>> refresh(HttpServletRequest request) {
+        LoginResponse responseDto = authService.refreshAccessToken(request);
         return ResponseEntity.ok(ApiResponseForm.success(responseDto, "액세스 토큰 재발급 성공"));
     }
+
+    @PostMapping("/verification")
+    public ResponseEntity<ApiResponseForm> sendVerificationCode(@RequestBody EmailRequest request) throws IOException {
+        authService.sendVerificationCode(request.getEmail());
+        return ResponseEntity.ok(ApiResponseForm.success(null));
+    }
+
+    @PostMapping("/verification/confirm")
+    public ResponseEntity<ApiResponseForm<Boolean>> verifyVerificationCode(@RequestBody EmailVerificationRequest request) {
+        boolean isVerified = authService.verifyVerificationCode(request.getEmail(), request.getCode());
+        return ResponseEntity.ok(ApiResponseForm.success(isVerified, "인증번호 확인 결과"));
+    }
+
+    @PostMapping("/password/change")
+    public ResponseEntity<ApiResponseForm<Void>> changePassword(@RequestBody ChangePasswordRequest requestDto) {
+        authService.changePassword(requestDto);
+        return ResponseEntity.ok(ApiResponseForm.success(null, "비밀번호 변경 성공"));
+    }
+
 }

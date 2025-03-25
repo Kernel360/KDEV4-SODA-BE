f591bfcfbf85742be1efc0179f9f3fa75147f313
seoyeon-jung <jungluchia1204@gmail.com>
2025-03-25 14:52:34 +0900

    feat: DTO 생성 (#10)

--- src/main/java/com/soda/article/domain/ArticleDTO.java
+++ src/main/java/com/soda/article/domain/ArticleDTO.java
@@ -1,63 +1,69 @@
 package com.soda.article.domain;
 
-import com.soda.article.entity.Article;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.soda.article.enums.ArticleStatus;
 import com.soda.article.enums.PriorityType;
-import com.soda.common.BaseEntity;
-import com.soda.member.entity.Member;
-import com.soda.project.entity.Stage;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ArticleDTO {
-    private Long id; // 게시글 ID
-    private String title; // 제목
-    private String content; // 내용
-    private PriorityType priority; // 우선순위 (LOW, MEDIUM, HIGH)
-    private LocalDateTime deadline; // 마감일 (선택사항)
-    private ArticleStatus status; // 상태 (기본값: PENDING)
-    private Long memberId; // 작성자 ID (member_id)
-    private Long stageId; // 프로젝트 단계 ID (stage_id)
-    private Long parentArticleId; // 부모 게시글 ID (null 이면 부모 게시글이고 추가되어있으면 답글)
-    private List<Long> fileList; // 첨부파일 리스트
-    private List<Long> linkList; // 첨부 링크 리스트
-    private List<Long> commentList; // 댓글 리스트
-    private List<Long> childArticleList; // 자식 게시글 리스트
+    private Long id;
 
-    // Entity → DTO 변환
-    public static ArticleDTO fromEntity(Article article) {
-        return ArticleDTO.builder()
-                .id(article.getId())
-                .title(article.getTitle())
-                .content(article.getContent())
-                .priority(article.getPriority())
-                .deadline(article.getDeadline())
-                .status(article.getStatus())
-                .memberId(article.getMember().getId())
-                .stageId(article.getStage().getId())
-                .parentArticleId(article.getParentArticle().getId())
-                .fileList(article.getArticleFileList().stream().map(BaseEntity::getId).toList()) // TODO file 형식으로 변경하기
-                .linkList(article.getArticleLinkList().stream().map(BaseEntity::getId).toList()) // TODO link 형식으로 변경하기
-                .commentList(article.getCommentList().stream().map(BaseEntity::getId).toList())
-                .childArticleList(article.getChildArticles().stream().map(BaseEntity::getId).toList())
-                .build();
+    @NotNull(message = "Title is required")
+    private String title;
+
+    @NotNull(message = "Content is required")
+    private String content;
+
+    @NotNull(message = "Stage Id is required")
+    private Long stageId;
+
+    private PriorityType priority;
+    private LocalDateTime deadLine;
+    private ArticleStatus status; // article 생성 시 default PENDING
+
+    private Long parentArticleId;
+    private List<FileDTO> fileList;
+    private List<LinkDTO> linkList;
+    private List<CommentDTO> commentList;
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class FileDTO {
+        private String name;
+        private String url;
     }
 
-    // DTO → Entity 변환 (request)
-    public Article toEntity(Member member, Stage stage) {
-        return Article.builder()
-                .title(this.title)
-                .content(this.content)
-                .priority(this.priority)
-                .deadline(this.deadline)
-                .status(ArticleStatus.PENDING)
-                .member(member)
-                .stage(stage)
-                .build();
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LinkDTO {
+        private String urlAddress;
+        private String urlDescription;
     }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class CommentDTO {
+        private Long id;
+        private String content;
+        private Long memberId;
+        private LocalDateTime createdDate;
+    }
+
 }
---
+++ src/main/java/com/soda/article/domain/ArticleFileDTO.java
@@ -0,0 +1,15 @@
+package com.soda.article.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArticleFileDTO {
+    private String name;
+    private String url;
+}

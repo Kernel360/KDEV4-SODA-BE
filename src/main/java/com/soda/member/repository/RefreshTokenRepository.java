443cb01a2936ec96edd0be89d85c5e0bd4d6c519
yoodab <yoondab2166@gmail.com>
2025-03-25 12:48:34 +0900

    refactor:리프레시 토큰 저장소 redis로 변경(#13)

--- src/main/java/com/soda/member/repository/RefreshTokenRepository.java
+++ src/main/java/com/soda/member/repository/RefreshTokenRepository.java
@@ -1,12 +1,37 @@
 package com.soda.member.repository;
 
-import com.soda.member.entity.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
-    Optional<RefreshToken> findByToken(String refreshToken);
+@Repository
+@RequiredArgsConstructor
+public class RefreshTokenRepository {
 
-    void deleteByAuthId(String authId);
+    private final StringRedisTemplate redisTemplate;
+
+    @Value("${jwt.refresh.expiration}")
+    private long refreshTokenValidTime;
+
+    public void save(String authId, String refreshToken) {
+        redisTemplate.opsForValue().set(
+                "refresh_token:" + authId,
+                refreshToken,
+                refreshTokenValidTime / 1000,
+                TimeUnit.SECONDS
+        );
+    }
+
+    public Optional<String> findByAuthId(String authId) {
+        String token = redisTemplate.opsForValue().get("refresh_token:" + authId);
+        return Optional.ofNullable(token);
+    }
+
+    public void deleteByAuthId(String authId) {
+        redisTemplate.delete("refresh_token:" + authId);
+    }
 }

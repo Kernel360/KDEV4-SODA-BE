ff8142db4259fda49495603a5a5b1837c61037db
seoyeon-jung <jungluchia1204@gmail.com>
2025-03-27 15:07:28 +0900

    mod: DTO 로직 수정 (#34)

--- src/main/java/com/soda/article/domain/CommentDTO.java
+++ src/main/java/com/soda/article/domain/CommentDTO.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
@@ -20,15 +21,15 @@ public class CommentDTO {
     private Long parentCommentId; // 부모 댓글 ID (대댓글의 경우)
     private List<CommentDTO> childComments; // 자식 댓글 리스트
 
-    @Getter
-    @Builder
-    public static class MemberDTO {
-        private Long id; // 회원 ID
-        private String name; // 회원 이름
-    }
-
     // 엔티티를 DTO로 변환하는 메소드
     public static CommentDTO fromEntity(Comment comment) {
+        // 자식 댓글을 포함시켜서 반환
+        List<CommentDTO> childCommentDTOs = comment.getChildComments() != null ?
+                comment.getChildComments().stream()
+                        .map(CommentDTO::fromEntity)
+                        .collect(Collectors.toList()) :
+                null;
+
         return CommentDTO.builder()
                 .id(comment.getId())
                 .content(comment.getContent())
@@ -38,9 +39,26 @@ public class CommentDTO {
                         .build())
                 .articleId(comment.getArticle().getId())
                 .parentCommentId(comment.getParentComment() != null ? comment.getParentComment().getId() : null)
-                .childComments(comment.getChildComments().stream()
-                        .map(CommentDTO::fromEntity)
-                        .collect(java.util.stream.Collectors.toList()))
+                .childComments(childCommentDTOs) // 자식 댓글을 포함
                 .build();
+    }
+
+    // 자식 댓글을 설정
+    public CommentDTO withChildComments(List<CommentDTO> childComments) {
+        return CommentDTO.builder()
+                .id(this.id)
+                .content(this.content)
+                .member(this.member)
+                .articleId(this.articleId)
+                .parentCommentId(this.parentCommentId)
+                .childComments(childComments)
+                .build();
+    }
+
+    @Getter
+    @Builder
+    public static class MemberDTO {
+        private Long id; // 회원 ID
+        private String name; // 회원 이름
     }
 }

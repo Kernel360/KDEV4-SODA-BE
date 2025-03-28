2aa3990d009a54ba2be07a0654ee8779f723b39f
yoodab <yoondab2166@gmail.com>
2025-03-28 15:40:18 +0900

    mod: ErrorCode 수정 (#37)

--- src/main/java/com/soda/member/error/MemberErrorCode.java
+++ src/main/java/com/soda/member/error/MemberErrorCode.java
@@ -4,9 +4,8 @@ import com.soda.global.response.ErrorCode;
 import org.springframework.http.HttpStatus;
 
 public enum MemberErrorCode implements ErrorCode {
-    NOT_FOUND_MEMBER("2005", "멤버를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
-    DUPLICATE_AUTH_ID("2008", "이미 사용 중인 아이디입니다.", HttpStatus.BAD_REQUEST),
-    NOT_FOUND_COMPANY("2010", "회사를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST);
+    NOT_FOUND_MEMBER("2301", "멤버를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
+    DUPLICATE_AUTH_ID("2302", "이미 사용 중인 아이디입니다.", HttpStatus.BAD_REQUEST);
 
     private final String code;
     private final String message;
--- src/main/java/com/soda/project/error/StageErrorCode.java
+++ src/main/java/com/soda/project/error/StageErrorCode.java
@@ -1,19 +1,19 @@
-package com.soda.member.error;
+package com.soda.project.error;
 
 import com.soda.global.response.ErrorCode;
 import org.springframework.http.HttpStatus;
 
-public enum CompanyErrorCode implements ErrorCode {
+public enum StageErrorCode implements ErrorCode {
+    STAGE_LIMIT_EXCEEDED("2401", "Stage limit exceeded (maximum 10)", HttpStatus.BAD_REQUEST),
+    STAGE_NOT_FOUND("2402", "The stage does not exist", HttpStatus.NOT_FOUND),
 
-    NOT_FOUND_COMPANY("2201", "회사를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
-    DUPLICATE_COMPANY_NUMBER("2202", "중복된 사업자 등록번호 입니다.", HttpStatus.BAD_REQUEST),
-    NOT_FOUND_DELETED_COMPANY("2203", "삭제된 회사를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST);
+    ;
 
     private final String code;
     private final String message;
     private final HttpStatus httpStatus;
 
-    CompanyErrorCode(String code, String message, HttpStatus httpStatus) {
+    StageErrorCode (String code, String message, HttpStatus httpStatus) {
         this.code = code;
         this.message = message;
         this.httpStatus = httpStatus;

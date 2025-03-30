55df9c8d1fa3daec4ccd432deb8f8924c41a41d8
yoodab <yoondab2166@gmail.com>
2025-03-31 01:46:15 +0900

    refactor: StageController 주소 변경 (#37)

--- src/main/java/com/soda/project/controller/StageController.java
+++ src/main/java/com/soda/project/controller/StageController.java
@@ -12,33 +12,32 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/stages")
 @RestController
 @RequiredArgsConstructor
 public class StageController {
 
     private final StageService stageService;
 
-    @PostMapping("")
+    @PostMapping("/stages")
     public ResponseEntity<ApiResponseForm<StageResponse>> addStage(@RequestBody StageCreateRequest request) {
         StageResponse stage = stageService.addStage(request);
         return ResponseEntity.ok(ApiResponseForm.success(stage, "단계 추가 성공"));
     }
 
-    @GetMapping("/{projectId}")
+    @GetMapping("/projects/{projectId}/stages")
     public ResponseEntity<ApiResponseForm<List<StageReadResponse>>> getStages(@PathVariable Long projectId) {
         List<StageReadResponse> stageReadResponseList = stageService.getStages(projectId);
         return ResponseEntity.ok(ApiResponseForm.success(stageReadResponseList, "단계 조회 성공"));
     }
 
-    @PutMapping("/{stageId}/move")
+    @PutMapping("/stages/{stageId}/move")
     public ResponseEntity<ApiResponseForm<Void>> moveStage(@PathVariable Long stageId,
                                                            @RequestBody StageMoveRequest request) {
         stageService.moveStage(stageId, request);
         return ResponseEntity.ok(ApiResponseForm.success(null, "단계 이동 성공"));
     }
 
-    @DeleteMapping("/{stageId}")
+    @DeleteMapping("/stages/{stageId}")
     public ResponseEntity<ApiResponseForm<Void>> deleteStage(@PathVariable Long stageId) {
         stageService.deleteStage(stageId);
         return ResponseEntity.ok(ApiResponseForm.success(null, "단계 삭제 성공"));

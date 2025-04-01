9515678fc9f7bee01d98b1b9382464cec2137dfa
JunbeomKoreaUniv <ku2soon@korea.ac.kr>
2025-04-01 12:17:44 +0900

    refact: ResponseLink 전략 생성 (#56)

--- src/main/java/com/soda/request/error/ResponseErrorCode.java
+++ src/main/java/com/soda/request/error/ResponseErrorCode.java
@@ -7,7 +7,9 @@ public enum ResponseErrorCode implements ErrorCode {
     RESPONSE_NOT_FOUND("3101", "This response id is not found in Requests", HttpStatus.NOT_FOUND),
     USER_NOT_WRITE_RESPONSE("3102", "This user didn't write the response", HttpStatus.UNAUTHORIZED),
     RESPONSE_FILE_NOT_FOUND("3103", "This response_file is not found" , HttpStatus.NOT_FOUND),
-    USER_NOT_UPLOAD_FILE("3104", "This user didn't upload the response_file" , HttpStatus.UNAUTHORIZED ),;
+    USER_NOT_UPLOAD_FILE("3104", "This user didn't upload the response_file" , HttpStatus.UNAUTHORIZED ),
+    RESPONSE_LINK_NOT_FOUND("3105", "This response link is not found" , HttpStatus.NOT_FOUND ),
+    USER_NOT_UPLOAD_LINK("3106", "This user didn't upload the response_link" , HttpStatus.UNAUTHORIZED ),;
 
     private final String code;
     private final String message;
---
+++ src/main/java/com/soda/request/strategy/ResponseLinkStrategy.java
@@ -0,0 +1,70 @@
+package com.soda.request.strategy;
+
+import com.soda.common.link.dto.LinkUploadRequest;
+import com.soda.common.link.strategy.LinkStrategy;
+import com.soda.global.response.GeneralException;
+import com.soda.request.entity.Response;
+import com.soda.request.entity.ResponseLink;
+import com.soda.request.error.ResponseErrorCode;
+import com.soda.request.repository.ResponseLinkRepository;
+import com.soda.request.repository.ResponseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ResponseLinkStrategy implements LinkStrategy<Response, ResponseLink> {
+
+    private final ResponseRepository responseRepository;
+    private final ResponseLinkRepository responseLinkRepository;
+
+
+    @Override
+    public String getSupportedDomain() {
+        return "response";
+    }
+
+    @Override
+    public Response getDomainOrThrow(Long domainId) {
+        return responseRepository.findById(domainId)
+                .orElseThrow(() -> new GeneralException(ResponseErrorCode.RESPONSE_NOT_FOUND));
+    }
+
+    @Override
+    public void validateWriter(Long memberId, Response response) {
+        if (!response.getMember().getId().equals(memberId)) {
+            throw new GeneralException(ResponseErrorCode.USER_NOT_WRITE_RESPONSE);
+        }
+    }
+
+    @Override
+    public ResponseLink toEntity(LinkUploadRequest.LinkUploadDTO dto, Response response) {
+        return ResponseLink.builder()
+                .urlAddress(dto.getUrlAddress())
+                .urlDescription(dto.getUrlDescription())
+                .response(response)
+                .build();
+    }
+
+    @Override
+    public void saveAll(List<ResponseLink> entities) {
+        responseLinkRepository.saveAll(entities);
+    }
+
+    @Override
+    public ResponseLink getLinkOrThrow(Long linkId) {
+        return responseLinkRepository.findById(linkId)
+                .orElseThrow(() -> new GeneralException(ResponseErrorCode.RESPONSE_LINK_NOT_FOUND));
+    }
+
+    @Override
+    public void validateLinkUploader(Long memberId, ResponseLink link) {
+        if (!link.getResponse().getMember().getId().equals(memberId)) {
+            throw new GeneralException(ResponseErrorCode.USER_NOT_UPLOAD_LINK);
+        }
+    }
+}

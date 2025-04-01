5614062f4db9dd47cef12d33a7918db4402690fd
seoyeon-jung <jungluchia1204@gmail.com>
2025-04-01 12:00:51 +0900

    mod: request에 toEntity 메서드 추가 (#57)

--- src/main/java/com/soda/article/domain/article/ArticleCreateRequest.java
+++ src/main/java/com/soda/article/domain/article/ArticleCreateRequest.java
@@ -1,7 +1,11 @@
 package com.soda.article.domain.article;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.soda.article.entity.Article;
+import com.soda.article.enums.ArticleStatus;
 import com.soda.article.enums.PriorityType;
+import com.soda.member.entity.Member;
+import com.soda.project.entity.Stage;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -23,5 +27,18 @@ public class ArticleCreateRequest {
     private Long parentArticleId;           // 답글인 경우 필요함
     private List<ArticleFileDTO> fileList;
     private List<ArticleLinkDTO> linkList;
+
+    public Article toEntity(Member member, Stage stage, Article parentArticle) {
+        return Article.builder()
+                .title(this.title)
+                .content(this.content)
+                .priority(this.priority)
+                .deadline(this.deadLine)
+                .member(member)
+                .stage(stage)
+                .status(ArticleStatus.PENDING) // 기본 상태는 PENDING
+                .parentArticle(parentArticle) // 답글이 있으면 부모 게시글을 설정
+                .build();
+    }
 
 }

bea6b5dc2024a6aefd2370ecc590c35296908210
JunbeomKoreaUniv <ku2soon@korea.ac.kr>
2025-04-05 15:32:47 +0900

    refact: 데이터로그 조회하는 API호출 시 DTO로 반환되게 리팩토링 (#78)

--- src/main/java/com/soda/global/log/dataLog/controller/DataLogController.java
+++ src/main/java/com/soda/global/log/dataLog/controller/DataLogController.java
@@ -2,9 +2,9 @@ package com.soda.global.log.dataLog.controller;
 
 import com.soda.global.log.dataLog.domain.DataLog;
 import com.soda.global.log.dataLog.dto.DataLogSearchRequest;
+import com.soda.global.log.dataLog.dto.DataLogSearchResponse;
 import com.soda.global.log.dataLog.service.DataLogService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +18,7 @@ public class DataLogController {
     private final DataLogService dataLogService;
 
     @GetMapping("")
-    public Page<DataLog> getChangedLogs(DataLogSearchRequest condition, Pageable pageable) {
+    public DataLogSearchResponse<DataLog> getChangedLogs(DataLogSearchRequest condition, Pageable pageable) {
         return dataLogService.getChangeLogs(condition, pageable);
     }
 }

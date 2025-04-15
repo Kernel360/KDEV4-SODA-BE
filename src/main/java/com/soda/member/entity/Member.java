d431d7d97e78df7450cb414e53a293aeeddcdd9d
yoodab <yoondab2166@gmail.com>
2025-04-15 15:10:54 +0900

    refactor: authId unique 속성 추가(#129)

--- src/main/java/com/soda/member/entity/Member.java
+++ src/main/java/com/soda/member/entity/Member.java
@@ -23,7 +23,7 @@ public class Member extends BaseEntity {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String authId;
 
     @Column(nullable = false)

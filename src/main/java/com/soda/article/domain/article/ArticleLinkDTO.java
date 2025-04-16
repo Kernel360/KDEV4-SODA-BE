dda2248772d726f13912ced936704d6ae43ab3a1
seoyeon-jung <jungluchia1204@gmail.com>
2025-04-16 14:34:51 +0900

    fix: id, deleted 파일/링크DTO에 추가 (#133)
    [프론트 연동 시 id가 없어서 삭제가 안되는 오류를 해결하고, 삭제 여부를 확인해서 삭제된 파일/링크는 조회되지 않도록 하기 위해 추가하였습니다.]

--- src/main/java/com/soda/article/domain/article/ArticleLinkDTO.java
+++ src/main/java/com/soda/article/domain/article/ArticleLinkDTO.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @Builder
 public class ArticleLinkDTO {
-
+    private Long id;
     private String urlAddress;
     private String urlDescription;
+    private boolean deleted;
 }

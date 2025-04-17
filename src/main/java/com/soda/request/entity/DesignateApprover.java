73b9fb6ea664deb24a1ff9cd4cd670556f94122a
JunbeomKoreaUniv <ku2soon@korea.ac.kr>
2025-04-17 16:16:11 +0900

    feat: DesignateApprover 엔티티(Request-Member 중간테이블) 정의 (#141)

---
+++ src/main/java/com/soda/request/entity/DesignateApprover.java
@@ -0,0 +1,25 @@
+package com.soda.request.entity;
+
+import com.soda.common.BaseEntity;
+import com.soda.member.entity.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class DesignateApprover extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "request_id")
+    private Request request;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}

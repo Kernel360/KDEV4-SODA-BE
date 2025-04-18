ac799ea4ab1eb03c1c8f54ccaf4972b58d2adb52
seoyeon-jung <jungluchia1204@gmail.com>
2025-04-18 12:47:27 +0900

    refactor: CompanyProjectRole 리팩토링 (#138)
    - 회사와 멤버의 역할을 자동으로 매핑하기 위해 enum 파일을 수정했습니다.

--- src/main/java/com/soda/member/enums/CompanyProjectRole.java
+++ src/main/java/com/soda/member/enums/CompanyProjectRole.java
@@ -4,14 +4,17 @@ import lombok.Getter;
 
 @Getter
 public enum CompanyProjectRole {
-    DEV_COMPANY("개발사"),
-    CLIENT_COMPANY("고객사")
-    ;
+    DEV_COMPANY("개발사", MemberProjectRole.DEV_MANAGER, MemberProjectRole.DEV_PARTICIPANT),
+    CLIENT_COMPANY("고객사", MemberProjectRole.CLI_MANAGER, MemberProjectRole.CLI_PARTICIPANT);
 
     private final String description;
+    private final MemberProjectRole managerRole;
+    private final MemberProjectRole memberRole;
 
-    CompanyProjectRole(String description) {
+    CompanyProjectRole(String description, MemberProjectRole managerRole, MemberProjectRole memberRole) {
         this.description = description;
+        this.managerRole = managerRole;
+        this.memberRole = memberRole;
     }
 
 }

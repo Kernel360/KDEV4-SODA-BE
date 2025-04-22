836dacd2a6074505a1db2b3114a83adb7db8cc25
seoyeon-jung <jungluchia1204@gmail.com>
2025-04-22 16:34:36 +0900

    feat: custom reposttory에 프로젝트 유효성 검사 메서드 추가 (#170)

--- src/main/java/com/soda/article/repository/ArticleRepositoryCustom.java
+++ src/main/java/com/soda/article/repository/ArticleRepositoryCustom.java
@@ -1,11 +1,15 @@
 package com.soda.article.repository;
 
 import com.querydsl.core.Tuple;
+import com.soda.article.entity.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 
 public interface ArticleRepositoryCustom {
 
     Page<Tuple> findMyArticlesData(Long authorId, Long projectId, Pageable pageable);
 
+    Optional<Article> findByIdAndIsDeletedFalseWithMemberAndCompanyUsingQuerydsl(Long articleId);
 }

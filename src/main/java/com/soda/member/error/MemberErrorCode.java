95e1c168143153ad018dae7adf17cb22dd72a7d1
yoodab <yoondab2166@gmail.com>
2025-04-24 01:04:38 +0900

    feat: MemberErrorCode 추가 (#167)

--- src/main/java/com/soda/member/error/MemberErrorCode.java
+++ src/main/java/com/soda/member/error/MemberErrorCode.java
@@ -8,7 +8,10 @@ public enum MemberErrorCode implements ErrorCode {
     DUPLICATE_AUTH_ID("2302", "이미 사용 중인 아이디입니다.", HttpStatus.BAD_REQUEST),
     MEMBER_NOT_ADMIN("2303", "관리자가 아닙니다", HttpStatus.BAD_REQUEST),
     DUPLICATE_EMAIL("2304","이미 사용 중인 이메일입니다." ,HttpStatus.BAD_REQUEST ),
-    CANNOT_DEACTIVATE_SELF("2305","본인 계정은 비활성화 할 수 없습니다." ,HttpStatus.BAD_REQUEST );
+    CANNOT_DEACTIVATE_SELF("2305","본인 계정은 비활성화 할 수 없습니다." ,HttpStatus.BAD_REQUEST ),
+    INVALID_CURRENT_PASSWORD( "2306", "현재 비밀번호가 일치하지 않습니다.",HttpStatus.BAD_REQUEST),
+    NEW_PASSWORD_SAME_AS_OLD("2307", "새 비밀번호는 현재 비밀번호와 달라야 합니다.",HttpStatus.BAD_REQUEST),
+    ;
 
     private final String code;
     private final String message;

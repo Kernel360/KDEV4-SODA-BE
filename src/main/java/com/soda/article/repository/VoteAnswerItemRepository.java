42ae32ee81e111401898947311c2662ff603e69e
seoyeon-jung <jungluchia1204@gmail.com>
2025-04-23 11:19:58 +0900

    feat: querydsl 추가 (#201)

--- src/main/java/com/soda/article/repository/VoteAnswerItemRepository.java
+++ src/main/java/com/soda/article/repository/VoteAnswerItemRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VoteAnswerItemRepository extends JpaRepository<VoteAnswerItem, Long> {
+public interface VoteAnswerItemRepository extends JpaRepository<VoteAnswerItem, Long>, VoteAnswerItemRepositoryCustom {
 }

4983f923c5019ddeadc6eade9fe109b4023d53be
yoodab <yoondab2166@gmail.com>
2025-04-28 12:29:46 +0900

    feat: NotificationData 생성(#225)

---
+++ src/main/java/com/soda/notification/dto/NotificationData.java
@@ -0,0 +1,32 @@
+package com.soda.notification.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.soda.notification.enums.NotificationType;
+import lombok.Getter;
+
+/**
+ * @param type       알림 종류
+ * @param message    사용자에게 보여줄 메시지
+ * @param link       클릭 시 이동할 링크
+ * @param articleId  관련 게시글 ID
+ * @param commentId  관련 댓글 ID
+ * @param replyId    관련 대댓글 ID
+ * @param requestId  관련 작업 ID
+ * @param responseId 관련 승인요청 ID
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record NotificationData(NotificationType type, String message, String link, Long articleId, Long commentId,
+                               Long replyId, Long requestId, Long responseId) {
+
+    public static NotificationData forNewComment(String message, String link, Long articleId, Long commentId) {
+        return new NotificationData(NotificationType.NEW_COMMENT_ON_POST, message, link, articleId, commentId, null, null, null);
+    }
+
+    public static NotificationData forNewReplyOnPost(String message, String link, Long articleId, Long parentCommentId, Long replyId) {
+        return new NotificationData(NotificationType.NEW_REPLY_ON_POST, message, link, articleId, parentCommentId, replyId, null, null);
+    }
+
+    public static NotificationData forNewReplyToMyComment(String message, String link, Long articleId, Long parentCommentId, Long replyId) {
+        return new NotificationData(NotificationType.NEW_REPLY_TO_MY_COMMENT, message, link, articleId, parentCommentId, replyId, null, null);
+    }
+}

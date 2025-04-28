ef045ac1b5173271f31d759c8bf432fceca88edd
yoodab <yoondab2166@gmail.com>
2025-04-28 23:29:05 +0900

    feat: MemberNotificationService 생성(#225)

---
+++ src/main/java/com/soda/notification/service/MemberNotificationService.java
@@ -0,0 +1,18 @@
+package com.soda.notification.service;
+
+import com.soda.notification.entity.MemberNotification;
+import com.soda.notification.repository.MemberNotificationRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MemberNotificationService {
+    private final MemberNotificationRepository memberNotificationRepository;
+
+    public void save(MemberNotification memberNotification) {
+        memberNotificationRepository.save(memberNotification);
+    }
+}

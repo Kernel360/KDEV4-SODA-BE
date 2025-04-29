a11a99b541b6af66a22b5dba2e2d694bb4e1d407
yoodab <yoondab2166@gmail.com>
2025-04-30 01:17:08 +0900

    feat: 읽지 않은 알림 목록 조회 메서드 생성 (#222)

--- src/main/java/com/soda/notification/repository/MemberNotificationRepository.java
+++ src/main/java/com/soda/notification/repository/MemberNotificationRepository.java
@@ -1,7 +1,14 @@
 package com.soda.notification.repository;
 
 import com.soda.notification.entity.MemberNotification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberNotificationRepository extends JpaRepository<MemberNotification, Long> {
+
+    @EntityGraph(attributePaths = {"notification"})
+    Page<MemberNotification> findByMemberIdAndIsDeletedFalse(Long memberId, Pageable pageable);
+
 }
--- src/main/java/com/soda/notification/service/MemberNotificationService.java
+++ src/main/java/com/soda/notification/service/MemberNotificationService.java
@@ -4,6 +4,8 @@ import com.soda.notification.entity.MemberNotification;
 import com.soda.notification.repository.MemberNotificationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,5 +16,9 @@ public class MemberNotificationService {
 
     public void save(MemberNotification memberNotification) {
         memberNotificationRepository.save(memberNotification);
+    }
+
+    public Page<MemberNotification> findByMemberIdAndIsDeletedFalse(Long userId, Pageable pageable) {
+        return memberNotificationRepository.findByMemberIdAndIsDeletedFalse(userId, pageable);
     }
 }

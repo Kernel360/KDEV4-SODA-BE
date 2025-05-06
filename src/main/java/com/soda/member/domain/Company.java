1f8112a290bdc16078af42ffb0befada8e7feef8
yoodab <yoondab2166@gmail.com>
2025-05-07 02:08:20 +0900

    refactor: Company 생성,상태 관리 로직 추가 (#301)

--- src/main/java/com/soda/member/domain/Company.java
+++ src/main/java/com/soda/member/domain/Company.java
@@ -1,12 +1,13 @@
 package com.soda.member.domain;
 
 import com.soda.common.BaseEntity;
+import com.soda.member.interfaces.dto.company.CompanyCreateRequest;
 import com.soda.member.interfaces.dto.company.CompanyUpdateRequest;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,9 +17,7 @@ import java.util.List;
 
 @Getter
 @Entity
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Company extends BaseEntity {
 
     @Column(nullable = false)
@@ -41,28 +40,56 @@ public class Company extends BaseEntity {
     @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
     private List<Member> memberList = new ArrayList<>();
 
-    // Entity 수정 메서드
-    public void updateCompany(CompanyUpdateRequest request) {
-        if (request.getName() != null) {
-            this.name = request.getName();
-        }
-        if (request.getOwnerName() != null) {
-            this.ownerName = request.getOwnerName();
-        }
-        if (request.getPhoneNumber() != null) {
-            this.phoneNumber = request.getPhoneNumber();
-        }
-        if(request.getCompanyNumber() != null){
-            this.companyNumber = request.getCompanyNumber();
-        }
-        if (request.getAddress() != null) {
-            this.address = request.getAddress();
-        }
-        if (request.getDetailAddress() != null) {
-            this.detailAddress = request.getDetailAddress();
-        }
+    @Builder
+    public Company(String name, String phoneNumber, String ownerName,
+            String companyNumber, String address, String detailAddress) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.ownerName = ownerName;
+        this.companyNumber = companyNumber;
+        this.address = address;
+        this.detailAddress = detailAddress;
     }
+
+    public static Company create(CompanyCreateRequest request) {
+        return Company.builder()
+                .name(request.getName())
+                .phoneNumber(request.getPhoneNumber())
+                .ownerName(request.getOwnerName())
+                .companyNumber(request.getCompanyNumber())
+                .address(request.getAddress())
+                .detailAddress(request.getDetailAddress())
+                .build();
+    }
+
+    public void update(CompanyUpdateRequest request) {
+        if (request.getName() != null)
+            this.name = request.getName();
+        if (request.getPhoneNumber() != null)
+            this.phoneNumber = request.getPhoneNumber();
+        if (request.getOwnerName() != null)
+            this.ownerName = request.getOwnerName();
+        if (request.getCompanyNumber() != null)
+            this.companyNumber = request.getCompanyNumber();
+        if (request.getAddress() != null)
+            this.address = request.getAddress();
+        if (request.getDetailAddress() != null)
+            this.detailAddress = request.getDetailAddress();
+    }
+
     public void delete() {
         this.markAsDeleted();
+    }
+
+    public void restore() {
+        this.markAsActive();
+    }
+
+    public void addMember(Member member) {
+        this.memberList.add(member);
+    }
+
+    public void removeMember(Member member) {
+        this.memberList.remove(member);
     }
 }

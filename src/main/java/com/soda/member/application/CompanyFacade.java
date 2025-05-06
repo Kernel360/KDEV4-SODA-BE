a54944434d8456da42a0eacd49671df2bda4509e
yoodab <yoondab2166@gmail.com>
2025-05-07 01:34:10 +0900

    refactor: CompanyFacade 생성 (#301)

---
+++ src/main/java/com/soda/member/application/CompanyFacade.java
@@ -0,0 +1,62 @@
+package com.soda.member.application;
+
+import com.soda.global.log.data.annotation.LoggableEntityAction;
+import com.soda.member.domain.Company;
+import com.soda.member.domain.CompanyService;
+import com.soda.member.interfaces.dto.CompanyCreationTrend;
+import com.soda.member.interfaces.dto.company.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CompanyFacade {
+    private final CompanyService companyService;
+
+    @LoggableEntityAction(action = "CREATE", entityClass = Company.class)
+    @Transactional
+    public CompanyResponse createCompany(CompanyCreateRequest request) {
+        return companyService.createCompany(request);
+    }
+
+    public List<CompanyResponse> getAllCompanies() {
+        return companyService.getAllCompanies();
+    }
+
+    public CompanyResponse getCompanyById(Long id) {
+        return companyService.getCompanyById(id);
+    }
+
+    public Company getCompany(Long id) {
+        return companyService.getCompany(id);
+    }
+
+    @LoggableEntityAction(action = "UPDATE", entityClass = Company.class)
+    @Transactional
+    public CompanyResponse updateCompany(Long id, CompanyUpdateRequest request) {
+        return companyService.updateCompany(id, request);
+    }
+
+    @LoggableEntityAction(action = "DELETE", entityClass = Company.class)
+    @Transactional
+    public void deleteCompany(Long id) {
+        companyService.deleteCompany(id);
+    }
+
+    @Transactional
+    public CompanyResponse restoreCompany(Long id) {
+        return companyService.restoreCompany(id);
+    }
+
+    public List<MemberResponse> getCompanyMembers(Long companyId) {
+        return companyService.getCompanyMembers(companyId);
+    }
+
+    public List<CompanyCreationTrend> getCompanyCreationTrend(CompanyTrendSearchCondition condition) {
+        return companyService.getCompanyCreationTrend(condition);
+    }
+}

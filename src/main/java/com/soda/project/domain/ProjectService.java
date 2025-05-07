8e7926467741f5ab1c87d13b52449654979866f6
seoyeon-jung <jungluchia1204@gmail.com>
2025-05-07 10:00:15 +0900

    refactor: 서비스에서 트랜잭션 삭제 (#283)

--- src/main/java/com/soda/project/domain/ProjectService.java
+++ src/main/java/com/soda/project/domain/ProjectService.java
@@ -12,13 +12,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
-@Transactional(readOnly = true)
 @Service
 @RequiredArgsConstructor
 public class ProjectService {

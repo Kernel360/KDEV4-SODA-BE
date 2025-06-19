5349c3c78a8b02c6c3572078aeaab3180796af33
JunbeomKoreaUniv <ku2soon@korea.ac.kr>
2025-06-19 13:57:40 +0900

    feat: 캐시 무효화시 현재 member가 승인권자인 승인요청들도 무효화되게 수정(#357)

--- src/main/java/com/soda/project/application/stage/request/RequestFacade.java
+++ src/main/java/com/soda/project/application/stage/request/RequestFacade.java
@@ -1,5 +1,6 @@
 package com.soda.project.application.stage.request;
 
+import com.soda.common.CacheEvictionHelper;
 import com.soda.global.log.data.annotation.LoggableEntityAction;
 import com.soda.member.domain.member.Member;
 import com.soda.member.domain.member.MemberService;
@@ -8,9 +9,9 @@ import com.soda.project.domain.stage.Stage;
 import com.soda.project.domain.stage.StageService;
 import com.soda.project.domain.stage.request.Request;
 import com.soda.project.domain.stage.request.RequestService;
+import com.soda.project.domain.stage.request.approver.ApproverDesignation;
 import com.soda.project.interfaces.stage.request.dto.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -30,56 +31,69 @@ public class RequestFacade {
     private final ProjectValidator projectValidator;
     private final RequestValidator requestValidator;
 
+    private final CacheEvictionHelper cacheEvictionHelper;
+
     @LoggableEntityAction(action = "CREATE", entityClass = Request.class)
     @Transactional
-    @CacheEvict(value = "myRequests",
-            key = "'member:' + #memberId + ':page:0:size:3'",
-            cacheManager = "myCacheManager")
     public RequestCreateResponse createRequest(Long memberId, RequestCreateRequest requestCreateRequest) {
         Member member = memberService.getMemberWithProjectOrThrow(memberId);
         Stage stage = stageService.getStageOrThrow(requestCreateRequest.getStageId());
         projectValidator.validateProjectDevAuthority(member, requestCreateRequest.getProjectId());
+        requestCacheEvictForCreate(memberId, requestCreateRequest.getMembers());
 
         return requestService.createRequest(member, stage, requestCreateRequest);
     }
 
     @LoggableEntityAction(action = "CREATE", entityClass = Request.class)
     @Transactional
-    @CacheEvict(value = "myRequests",
-            key = "'member:' + #memberId + ':page:0:size:3'",
-            cacheManager = "myCacheManager")
     public RequestCreateResponse createReRequest(Long memberId, Long requestId, ReRequestCreateRequest reRequestCreateRequest) {
         Member member = memberService.getMemberWithProjectOrThrow(memberId);
         Request parentRequest = requestService.getRequestOrThrow(requestId);
         Stage stage = stageService.getStageOrThrow(parentRequest.getStage().getId());
         projectValidator.validateProjectDevAuthority(member, stage.getProject().getId());
         requestValidator.validateRequestStatus(parentRequest);
+        requestCacheEvictForCreate(memberId, reRequestCreateRequest.getMembers());
 
         return requestService.createReRequest(requestId, member, stage, reRequestCreateRequest);
     }
 
+    private void requestCacheEvictForCreate(Long memberId, List<MemberAssignDTO> members) {
+        cacheEvictionHelper.evictMyRequestPage0Size3(memberId);
+
+        for (MemberAssignDTO memberAssign : members) {
+            Long approverId = memberAssign.getId();
+            if (approverId != null) {
+                cacheEvictionHelper.evictMyRequestPage0Size3(approverId);
+            }
+        }
+    }
+
     @LoggableEntityAction(action = "UPDATE", entityClass = Request.class)
     @Transactional
-    @CacheEvict(value = "myRequests",
-            key = "'member:' + #memberId + ':page:0:size:3'",
-            cacheManager = "myCacheManager")
     public RequestUpdateResponse updateRequest(Long memberId, Long requestId, RequestUpdateRequest requestUpdateRequest) {
         Request request = requestService.getRequestOrThrow(requestId);
         requestValidator.validaRequestWriter(memberId, request);
+        requestCacheEvict(memberId, request);
 
         return requestService.updateRequest(request, requestUpdateRequest);
     }
 
     @LoggableEntityAction(action = "DELETE", entityClass = Request.class)
     @Transactional
-    @CacheEvict(value = "myRequests",
-            key = "'member:' + #memberId + ':page:0:size:3'",
-            cacheManager = "myCacheManager")
     public RequestDeleteResponse deleteRequest(Long memberId, Long requestId) {
         Request request = requestService.getRequestOrThrow(requestId);
         requestValidator.validaRequestWriter(memberId, request);
+        requestCacheEvict(memberId, request);
 
         return requestService.deleteRequest(request);
+    }
+
+    private void requestCacheEvict(Long memberId, Request request) {
+        cacheEvictionHelper.evictMyRequestPage0Size3(memberId);
+
+        for (ApproverDesignation approver : request.getApprovers()) {
+            cacheEvictionHelper.evictMyRequestPage0Size3(approver.getMember().getId());
+        }
     }
 
     public Page<RequestDTO> findRequests(Long projectId, GetRequestCondition condition, Pageable pageable) {
